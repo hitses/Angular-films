fbc5c12dd989463187ac6cd955805077bf68f824
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-07-10 12:06:03 +0200

    Router and boilerplate configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesCategoryComponent } from './movies-category/movies-category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'movies/:category', component: MoviesCategoryComponent},
+  {path: '**', redirectTo: 'movies/populares'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
