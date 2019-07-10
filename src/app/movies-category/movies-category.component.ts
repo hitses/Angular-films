46f36b30eeaf6e16dfd4c222fe8bc617851d7716
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-07-10 12:59:38 +0200

    Category parameter subscribed

--- src/app/movies-category/movies-category.component.ts
+++ src/app/movies-category/movies-category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movies-category',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies-category.component.scss']
 })
 export class MoviesCategoryComponent implements OnInit {
+  category: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.category = params.category.replace('_', ' ');
+    });
   }
 
 }
